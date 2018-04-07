7246dc973460ae5be3195f45fd216eb7f434d040
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-04-07 23:32:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile('./index.html')
 })
 
 io.on('connection', (socket) => {
