70fc54d615304865a414473b1ec68a47fb12b58d
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-05-02 13:05:07 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
+//Делаем импорт
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+
+//Запускаем рутовый маршрут
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+
+//Запускаем сокет
 io.on('connection', (socket) => {
 
-    var user = Date.now();
-    io.emit('message', `User ${user} connected`)
+    //Задаем имя юзера и цвет при подключении
+    var user = {
+        name: Date.now(),
+        color: `rgba(${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},0.5)`
+    }
 
+
+    //Отправить всем сообщение, что юзер подконнектился
+    io.emit('message', {
+        text: `User ${user.name} connected`,
+        color: user.color
+    })
+
+
+    //Когда кто-то отправляет сообщения
     socket.on('send', (msg) => {
-        io.emit('message',`${user}: ${msg}`)
+        io.emit('message', {
+            text: `${user.name}: ${msg}`,
+            color: user.color
+        })
     })
 
+
+    //Когда кто-то меняет имя
     socket.on('name', (name) => {
-        io.emit('message',`${user} changed name to ${name}`)
-        user = name
+        io.emit('message', {
+            text: `${user.name} changed name to ${name}`,
+            color: user.color
+        })
+        user.name = name
     })
 
+    //Когда кто-то дисконнектится
     socket.on('disconnect', () => {
-        io.emit('message', `User ${user} disconnected`)
+        io.emit('message', {
+            text: `User ${user.name} disconnected`,
+            color: user.color
+        })
     });
 
+    //Когда кто-то печатает
     socket.on('typing', (name) => {
-        socket.broadcast.emit('typing',`${user} is typing...`)
+        socket.broadcast.emit('typing',`${user.name} is typing...`)
     })
 
 })
 
-http.listen((process.env.PORT || 5000), () => console.log('Started server'))
-
+http.listen(3000, () => console.log('Started server'))
