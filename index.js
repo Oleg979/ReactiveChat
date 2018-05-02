707b0a5ce2508765a894dca3f939b3a9a36333f4
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-05-02 13:09:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-//Делаем импорт
+
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 
-//Запускаем рутовый маршрут
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 
-//Запускаем сокет
+
 io.on('connection', (socket) => {
 
-    //Задаем имя юзера и цвет при подключении
     var user = {
         name: Date.now(),
         color: `rgba(${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},0.5)`
     }
 
 
-    //Отправить всем сообщение, что юзер подконнектился
     io.emit('message', {
         text: `User ${user.name} connected`,
         color: user.color
     })
 
 
-    //Когда кто-то отправляет сообщения
     socket.on('send', (msg) => {
         io.emit('message', {
             text: `${user.name}: ${msg}`,
@@ -36,7 +33,6 @@ io.on('connection', (socket) => {
     })
 
 
-    //Когда кто-то меняет имя
     socket.on('name', (name) => {
         io.emit('message', {
             text: `${user.name} changed name to ${name}`,
@@ -45,7 +41,6 @@ io.on('connection', (socket) => {
         user.name = name
     })
 
-    //Когда кто-то дисконнектится
     socket.on('disconnect', () => {
         io.emit('message', {
             text: `User ${user.name} disconnected`,
@@ -53,7 +48,6 @@ io.on('connection', (socket) => {
         })
     });
 
-    //Когда кто-то печатает
     socket.on('typing', (name) => {
         socket.broadcast.emit('typing',`${user.name} is typing...`)
     })
