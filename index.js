6b23a984b995183e399dfdffbd40deeda00d5aee
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-06-24 15:16:23 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,86 @@
+//Делаем импорт
 var app = require('express')()
 var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
 
-http.listen(3000);
+const pushAndEmit = msg => {
+    msgs.push(msg)
+    io.emit('message', msg)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Запускаем рутовый маршрут
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+let msgs = []
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Запускаем сокет
+io.on('connection', (socket) => {
+
+    //Задаем имя юзера и цвет при подключении
+    var user = {
+        name: Date.now(),
+        color: `rgba(${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},${Math.floor(Math.random() * (256))},0.5)`
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Создать сообщение и добавить в бд
+    let msg = {
+        text: `User ${user.name} connected`,
+        color: user.color
+    }
+    msgs.push(msg)
+
+    //Отправить новому юзеру всю историю сообщений
+    msgs.map(msg => socket.emit('message', msg))
+
+    //Отправить всем сообщение, что юзер подконнектился
+    socket.broadcast.emit('message', msg)
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //Когда кто-то отправляет сообщения
+    socket.on('send', msg => {
+        pushAndEmit({
+            text: `${user.name}: ${msg}`,
+            color: user.color
+        })
+    })
+
+
+    //Когда кто-то меняет имя
+    socket.on('name', name => {
+        pushAndEmit({
+            text: `${user.name} changed name to ${name}`,
+            color: user.color
+        })
+        user.name = name
+    })
+
+    //Когда кто-то дисконнектится
+    socket.on('disconnect', () => {
+        pushAndEmit({
+            text: `User ${user.name} disconnected`,
+            color: user.color
+        })
+    })
+
+    //Когда кто-то печатает
+    socket.on('typing', (name) => {
+        socket.broadcast.emit('typing',`${user.name} is typing...`)
+    })
+
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+http.listen(3000, () => console.log('Started server'))
